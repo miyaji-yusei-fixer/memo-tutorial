945d92a011236c83ab004eeed65a5d9fe2f72531
miyaji-yusei-fixer <miyaji.yusei@fixer.co.jp>
2024-01-23 04:08:38 +0900

    :zap: モバイルアプリのように表示する

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,6 +10,8 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
+      // iPhoneでモバイルアプリのように表示する
+      { name: "apple-mobile-web-app-capable", content: "yes" }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
